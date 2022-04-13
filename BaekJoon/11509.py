635e4e58cd046a0364dc85914df7c6a518cd7506
isayaksh <isayaksh@gmail.com>
2022-04-13 20:26:38 +0900

    [11509] 풍선 맞추기

--- BaekJoon/11509.py
+++ BaekJoon/11509.py
@@ -2,10 +2,12 @@
 from sys import stdin
 N = int(stdin.readline())
 H = list(map(int,stdin.readline().split()))
-A = [0 for _ in range(N+1)]
-for h in H:
-  # 현재 층에 화살이 있는 경우
-  if A[h]:
-    A[h] -= 1
-  A[h-1] += 1
-print(sum(A))
+Cur = [0 for _ in range(1000001)] # 현재 화살의 위치
+count = 0
+for i in range(N):
+  if Cur[H[i]] == 0: # 풍선의 위치에 화살이 없다면
+    count += 1
+  else: # 풍선의 위치에 화살이 있다면
+    Cur[H[i]] -= 1
+  Cur[H[i]-1] += 1 # 현재 풍선 위치 -1 에 화살 위치
+print(count)
