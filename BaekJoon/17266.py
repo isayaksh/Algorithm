41d0caaa546fec45bfee0585a50c969d2111cfb6
isayaksh <isayaksh@gmail.com>
2023-08-23 20:05:32 +0900

    [BOJ] 17266 어두운 굴다리

---
+++ BaekJoon/17266.py
@@ -0,0 +1,33 @@
+# BOJ 17266 어두운 굴다리
+# https://www.acmicpc.net/problem/17266
+
+from sys import stdin
+
+def solution(N, M, location):
+    answer = 0
+    
+    def possibleLightRange(lightRange):
+        if location[0] > lightRange or location[-1] + lightRange < N:
+            return False
+        for i in range(M-1):
+            if location[i+1] - location[i] > lightRange*2:
+                return False
+        return True
+
+    start, end = 0, N
+    while start < end:
+        mid = (start + end) // 2
+        if possibleLightRange(mid):
+            end = mid
+            answer = mid
+        else:
+            start = mid+1
+
+    return answer
+
+N = int(stdin.readline())
+M = int(stdin.readline())
+location = list(map(int,stdin.readline().split()))
+
+res = solution(N, M, location)
+print(res)
